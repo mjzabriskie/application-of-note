6c870967bb753d1375e63821bb933ab614e99d16
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-05-25 23:42:29 -0600

    added function to write updated notes object (db.json) to the db directory

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,19 +4,20 @@ const { notes } = require('../../db/db.json');
 const router = require('express').Router();
 const path = require('path');
 
+//api endpoint to get notes from db.json
 router.get('/notes', (req, res) => {
     let results = notes;
     res.json(results);
 });
 
-router.get('/notes/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
-});
+// router.get('/notes/:id', (req, res) => {
+//     const result = findById(req.params.id, notes);
+//     if (result) {
+//         res.json(result);
+//     } else {
+//         res.send(404);
+//     }
+// });
 
 router.post('/notes', (req, res) => {
     let note = req.body;
@@ -24,7 +25,7 @@ router.post('/notes', (req, res) => {
     //set random id using nanoid
     note.id = nanoid();
 
-    //if any data in note is incorrect, send 400 error back
+    //if data in note is missing, send 400 error back
     if (!note) {
         res.status(400).send('Add all information and try again.');
     } else {
@@ -37,9 +38,9 @@ router.post('/notes', (req, res) => {
     }
 });
 
-function findById(id, notesArray) {
-    const result = notesArray.filter(note => note.id === id)[0];
-    return result;
-}
+// function findById(id, notesArray) {
+//     const result = notesArray.filter(note => note.id === id)[0];
+//     return result;
+// }
 
 module.exports = router;
